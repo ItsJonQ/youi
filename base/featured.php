305e03e2e9eca07d34ab844374f7716d02cd846f
Jon Q <jquach@technobuffalo.com>
2013-05-27 15:32:56 -0400

    Added Font Smoothing

--- base/featured.php
+++ base/featured.php
@@ -2,7 +2,7 @@
 	<span class="placeholder-image" style="padding-bottom: 35%;"></span>
 	<div class="container">
 		<div class="row">
-			<h2 class="span6 block-title">Proin volutpat nunc id mauris venenatis volutpat. </h2>
+			<h2 class="span8 block-title">Proin volutpat nunc id mauris venenatis volutpat. </h2>
 		</div>
 	</div>
 </div>
