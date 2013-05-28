2a62b65254b0536f702c099dff4c0189ef67feb2
itsjonq@gmail.com <Jon@Jon-Quachs-MacBook-Pro.local>
2013-05-27 20:57:32 -0400

    Increased post count to 24

--- base/blog-full.php
+++ base/blog-full.php
@@ -2,17 +2,17 @@
 	<div class="row">
 		<?php include('post.php'); ?>
 
-		<?php include('post-mini.php'); ?>
-
-		<?php include('post-mini.php'); ?>
-
-		<?php include('post-mini.php'); ?>
-
 		<div class="span4"=>
 			<div class="block-wrap block-clear">
 				<?php include('ad-box.php'); ?>
 			</div>
 		</div>
+
+		<?php include('post-mini.php'); ?>
+
+		<?php include('post-mini.php'); ?>
+
+		<?php include('post-mini.php'); ?>
 
 		<?php include('post-mini.php'); ?>
 
@@ -43,6 +43,23 @@
 		<?php include('post-mini.php'); ?>
 
 		<?php include('post.php'); ?>
+
+		<?php include('post-mini.php'); ?>
+
+		<?php include('post-mini.php'); ?>
+
+		<?php include('post-mini.php'); ?>
+
+		<?php include('post.php'); ?>
+
+		<?php include('post-mini.php'); ?>
+
+		<?php include('post-mini.php'); ?>
+
+		<?php include('post-mini.php'); ?>
+
+		<?php include('post-mini.php'); ?>
+
 
 	</div>
 </div>
