a0809a2e5d64c553c8664fd6910a3fa56cec9c18
itsjonq@gmail.com <Jon@Jon-Quachs-MacBook-Pro.local>
2013-05-29 01:39:58 -0400

    Added jQuery + Scrollbar

--- base/js/main.js
+++ base/js/main.js
@@ -1,1 +1,24 @@
+$(document).ready(function(){
+	var wh = $(window).height();
+	var scrb = $('#scrollbar1');
+	scrb.css('height',wh-60);
+	scrb.tinyscrollbar();
 
+	scrb.mouseenter(function(){
+		$(this).find($('.scrollbar')).fadeIn('fast');
+	});
+	scrb.mouseleave(function(){
+		$(this).find($('.scrollbar')).fadeOut('fast');
+	});
+
+	$(window).load(function() {
+		var ot = scrb.offset().top;
+		$(window).scroll(function(){
+			if(ot < $(window).scrollTop()) {
+				scrb.css({position: 'fixed', top: '0px'});
+			} else {
+				scrb.css({position: 'static', top: '0px'});
+			}
+		});
+	});
+});
