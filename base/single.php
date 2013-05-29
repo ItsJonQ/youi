b4b0031bec403c2d4cc85e43e4ecf2787520ff1d
itsjonq@gmail.com <Jon@Jon-Quachs-MacBook-Pro.local>
2013-05-28 22:03:28 -0400

    Added Single

---
+++ base/single.php
@@ -0,0 +1,60 @@
+<?php include('header.php'); ?>
+
+<div class="span100 block-wrap big-featured">
+	<div class="block-image">
+		<img src="http://i.perezhilton.com/wp-content/uploads/2013/05/justin-bieber-segway-drugs-kids-neighbors-complain__oPt.jpg">
+	</div>
+</div>
+
+<div class="single container">
+	<div class="row">
+		<?php include('sidebar.php'); ?>
+		<div class="content span8">
+			<article class="block-wrap">
+				<div class="block-content">
+					<div class="block-meta">
+						<a href="#" class="box-button">Justin Bieber
+						</a>
+						<a href="#" class="box-button">Drugs
+						</a>
+						<a href="#" class="box-button">Legal Matters
+						</a>
+					</div>
+					<div class="block-title">
+						<h1>Justin Bieber Terrorizing Neighborhood With Drugs And Segways?!</h1>
+					</div>
+					<div class="block-meta">
+						By <strong>Perez Hilton</strong> | May 28, 2013
+					</div>
+					<div class="row">
+						<div class="block-article span8">
+
+							<p>We thought we would LOVE to live next to <strong><a href="http://perezhilton.com/category/justin-bieber" target="_blank">Justin Bieber</a></strong>! Now we're not so sure!</p>
+
+							<p>First, <em>Boyfriend</em> gets accused of recklessly <a href="http://perezhilton.com/2013-05-28-justin-bieber-reckless-driving-gets-cops-called-to-house#.UaUJRbZNBiY" target="_blank">driving</a> his Ferrari around the neighborhood (an <a href="http://perezhilton.com/2013-05-28-justin-bieber-ferrari-calabasas-tyler-the-creator" target="_blank">accusation</a> that has now found its way stamped on <strong>Tyler, The Creator</strong>)!</p>
+
+							<p>Now, according to multiple complaints from neighbors, the Calabasas kids have a new word for terror- Segway!</p>
+
+							<p>OK, it's probably not quite as bad a car! We're not sure it's even possible to wreck the mobile Weebles!</p>
+
+							<p>But the Biebs and his buddy were reportedly riding around the neighborhood while smoking joints- right in front of kids!</p>
+
+							<p>Add in the noise <a href="http://perez.ly/11vLSS8" target="_blank">complaints</a> about late night parties, and now living next to Justin doesn't seem like such a coup!</p>
+
+							<p>[<em>Image via <a href="http://rameypix.net/" target="_blank">Ramey Pix</a></em>.]
+							</p>
+
+						</div>
+					</div>
+				</div>
+				</div>
+			</article>
+			
+			<?php include('post-mini.php'); ?>
+
+			<?php include('post-mini.php'); ?>
+		</div>
+	</div>
+</div>
+
+<?php include('footer.php'); ?>
